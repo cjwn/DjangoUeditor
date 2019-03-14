ca64e1df94e9c4782cedc48effccdb6b5703cf34
cjwn <atom.cn@gmail.com>
2019-03-14 23:56:08 +0800

    Update urls.py
    适配 Django-1.11.3

--- DjangoUeditor/urls.py
+++ DjangoUeditor/urls.py
@@ -1,12 +1,20 @@
 #coding:utf-8
 from django import VERSION
 if VERSION[0:2]>(1,3):
-    from django.conf.urls import patterns, url
+    if VERSION [0:2]>(1,9):
+        from django.conf.urls import include, url
+    else:
+        from django.conf.urls import patterns, url
 else:
     from django.conf.urls.defaults import patterns, url
 
 from views import get_ueditor_controller
 
-urlpatterns = patterns('',
+if VERSION [0:2]>(1,9):
+    urlpatterns = [
+    url(r'^controller/$',get_ueditor_controller)
+]
+else:
+    urlpatterns = patterns('',
     url(r'^controller/$',get_ueditor_controller)
 )
